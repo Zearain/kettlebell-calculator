94e55206cb59113782b0a82b1389af24ed54df1e
Martin Bakke <martinba88@gmail.com>
2023-12-27 23:57:41 +0000

    Ugly UI

--- src/App/Program.cs
+++ src/App/Program.cs
@@ -7,6 +7,8 @@ using Kettlebell.Calc.Core;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
+using MudBlazor.Services;
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
@@ -14,5 +16,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddKettlebellCalculator(builder.Configuration);
+
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
